19e2b1f9893d3961f24587861653520cf28225ad
Meghdad <mmf9484943@gmail.com>
2024-06-16 16:16:27 +0330

    update to lms v02

---
+++ assets/dashboard.e234754b.js
@@ -0,0 +1,1 @@
+import{_ as r,c as o,b as _,m as a,d as t}from"./index.193d4fc4.js";const n={};function c(s,d){const e=a("router-view");return t(),o("div",null,[_(e)])}var u=r(n,[["render",c]]);export{u as default};
---
+++ assets/use-quasar.11a95063.js
@@ -0,0 +1,1 @@
+import{l as a,aD as r}from"./index.193d4fc4.js";function u(){return a(r)}export{u};
